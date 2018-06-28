9c0e82d1414829b3c8e3cfb6bc4fc52124fd6f81
nihalarjun-disa <40648835+nihalarjun-disa@users.noreply.github.com>
2018-06-28 09:18:12 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,351 @@
+onEvent("brand_button", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("button11", "click", function() {
+  setScreen("Home");
+});
+onEvent("button12", "click", function() {
+  setScreen("brandcategory2");
+});
+onEvent("button6", "click", function() {
+  setScreen("Home");
+});
+onEvent("Aprillia_image", "click", function() {
+  setScreen("Apriliabikes1");
+});
+onEvent("nextapriliabikes1", "click", function() {
+  setScreen("apriliabiles2");
+});
+onEvent("nextfromapriliabikes2", "click", function() {
+  setScreen("apriliabikes3");
+});
+onEvent("nexfromapriliabikes3", "click", function() {
+  setScreen("apriliabikes4");
+});
+onEvent("nextfromapriliabikes4", "click", function() {
+  setScreen("apriliabikes5");
+});
+onEvent("backfromapriliabikes5", "click", function() {
+  setScreen("apriliabikes4");
+});
+onEvent("backfromapriliabikes4", "click", function() {
+  setScreen("apriliabikes3");
+});
+onEvent("backfromapriliabikes3", "click", function() {
+  setScreen("apriliabiles2");
+});
+onEvent("backfromapriliabikes2", "click", function() {
+  setScreen("Apriliabikes1");
+});
+onEvent("backapriliabikes1", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("gallery_apriliashiver900", "click", function() {
+  setScreen("APRILIASHIVER900_gallerypage");
+});
+onEvent("galleryAprilia Dorsoduro 900", "click", function() {
+  setScreen("APRILIADORSODURO900_gallerypage");
+});
+onEvent("GALLERYAPRILIACAPONORD1200ABS", "click", function() {
+  setScreen("APRILIACAPONORD1200ABS_gallerypage");
+});
+onEvent("GALLERYDORSODURO1200ABS", "click", function() {
+  setScreen("APRILIADORSODURO1200ABS_gallerypage");
+});
+onEvent("galleryAprilia Tuono V4 1100", "click", function() {
+  setScreen("APRILIA TUONO V4 1100_gallerypage");
+});
+onEvent("galleryAprilia Caponord 1200 ABS Travel", "click", function() {
+  setScreen("APRILIA CAPONORD 1200 ABS TRAVEL_gallerypage");
+});
+onEvent("galleryAprilia Caponord 1200 Rally", "click", function() {
+  setScreen("APRILIA CAPONORD 1200 RALLY_gallerypage");
+});
+onEvent("gallery_APRILIARSV4RR", "click", function() {
+  setScreen("APRILIA RSV4 RR_gallerypage");
+});
+onEvent("galleryapriliaRSV4 RF", "click", function() {
+  setScreen("APRILIA RSV4 RF_gallerypage");
+});
+onEvent("BACKFROMAPRILIASHIVER900_gallery_page", "click", function() {
+  setScreen("Apriliabikes1");
+});
+onEvent("APRILIADORSODURO900_gallerypage", "click", function() {
+  setScreen("Apriliabikes1");
+});
+onEvent("GALLERYAPRILIACAPONORD1200ABS", "click", function() {
+  setScreen("APRILIACAPONORD1200ABS_gallerypage");
+});
+onEvent("GALLERYDORSODURO1200ABS", "click", function() {
+  setScreen("APRILIADORSODURO1200ABS_gallerypage");
+});
+onEvent("BACKFROMAPRILIACAPONORD1200ABS_gallery_page", "click", function() {
+  setScreen("apriliabiles2");
+});
+onEvent("BACKFROMAPRILIADORSODURO1200ABS_gallery_page", "click", function() {
+  setScreen("apriliabiles2");
+});
+onEvent("galleryAprilia Tuono V4 1100", "click", function() {
+  setScreen("APRILIA TUONO V4 1100_gallerypage");
+});
+onEvent("galleryAprilia Caponord 1200 ABS Travel", "click", function() {
+  setScreen("APRILIA CAPONORD 1200 ABS TRAVEL_gallerypage");
+});
+onEvent("BACKFROMAPRILIATUONOV41100GALLERY_PAGE", "click", function() {
+  setScreen("apriliabikes3");
+});
+onEvent("BACKFROMAPRILIA CAPONORD 1200 ABS TRAVEL_gallery_page", "click", function() {
+  setScreen("apriliabikes3");
+});
+onEvent("galleryAprilia Caponord 1200 Rally", "click", function() {
+  setScreen("APRILIA CAPONORD 1200 RALLY_gallerypage");
+});
+onEvent("gallery_APRILIARSV4RR", "click", function() {
+  setScreen("APRILIA RSV4 RR_gallerypage");
+});
+onEvent("BACKFROMAPRILIA CAPONORD 1200 RALLY_gallery_page", "click", function() {
+  setScreen("apriliabikes4");
+});
+onEvent("BACKFROMAPRILIA RSV4 RR_gallery_page", "click", function() {
+  setScreen("apriliabikes4");
+});
+onEvent("galleryapriliaRSV4 RF", "click", function() {
+  setScreen("APRILIA RSV4 RF_gallerypage");
+});
+onEvent("BACKFROMAPRILIA RSV4 RF_gallery_page", "click", function() {
+  setScreen("apriliabikes5");
+});
+onEvent("categoryfromaprilia5", "click", function() {
+  setScreen("Brand_category1");
+});
+
+onEvent("backfromHYOSUNG AQUILA PRO650_gallery", "click", function() {
+  setScreen("hyosungbikes2");
+});
+onEvent("backfromHYOSUNG AQUILA GV250_gallery", "click", function() {
+  setScreen("hyosungbikes1");
+});
+onEvent("backfromHYOSUNG GT250R_gallery", "click", function() {
+  setScreen("hyosungbikes1");
+});
+onEvent("backfromHYOSUNG GT650R_gallery", "click", function() {
+  setScreen("hyosungbikes2");
+});
+onEvent("nextfromhyosungbikes1", "click", function() {
+  setScreen("hyosungbikes2");
+});
+onEvent("backfromhyosungbikes1", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("categoryfromhyosung2", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("backfromhyosung2", "click", function() {
+  setScreen("hyosungbikes1");
+});
+onEvent("galleryhyosungaquilagv250", "click", function() {
+  setScreen("HYOSUNG AQUILA GV250_gallerypage");
+});
+onEvent("galleryhyosungGT250R", "click", function() {
+  setScreen("HYOSUNG GT250R_gallerypage");
+});
+onEvent("galleryhyosungGT650R", "click", function() {
+  setScreen("HYOSUNG GT650R_gallerypage");
+});
+onEvent("galleryhyosungAquila Pro650", "click", function() {
+  setScreen("HYOSUNG AQUILA PRO650_gallerypage");
+});
+onEvent("hyosung_image", "click", function() {
+  setScreen("hyosungbikes1");
+});
+onEvent("backfromktmbikes1", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("nextfromktmbikes1", "click", function() {
+  setScreen("ktmbikes2");
+});
+onEvent("KTM200DUKE_GALLERY", "click", function() {
+  setScreen("KTM 200 DUKE_gallerypage");
+});
+onEvent("backfromKTM 200 DUKE_gallery", "click", function() {
+  setScreen("ktmbikes1");
+});
+onEvent("backfromKTM RC200_gallery", "click", function() {
+  setScreen("ktmbikes1");
+});
+onEvent("galleryktm390duke", "click", function() {
+  setScreen("KTM 390 DUKE_gallerypage");
+});
+onEvent("backfromKTM 390 DUKE_gallery", "click", function() {
+  setScreen("ktmbikes2");
+});
+onEvent("galleryktm250duke", "click", function() {
+  setScreen("KTM 250 DUKE_gallerypage");
+});
+onEvent("backfromKTM 250 DUKE_gallery", "click", function() {
+  setScreen("ktmbikes2");
+});
+onEvent("backfromktmbikes2", "click", function() {
+  setScreen("ktmbikes1");
+});
+onEvent("nextfromktmbikes2", "click", function() {
+  setScreen("ktmbikes3");
+});
+onEvent("galleyktmrc390r", "click", function() {
+  setScreen("KTM RC390_gallerypage");
+});
+onEvent("backfromKTM RC390_gallery", "click", function() {
+  setScreen("ktmbikes3");
+});
+onEvent("backfromktmbikes3", "click", function() {
+  setScreen("ktmbikes2");
+});
+onEvent("categoryfromktmbikes3", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("KTM RC200_gallery", "click", function() {
+  setScreen("KTM RC200_gallerypage");
+});
+onEvent("mvaugustalogo", "click", function() {
+  setScreen("mvagustabikes1");
+});
+onEvent("gallerymvagustabrutale800", "click", function() {
+  setScreen("MV AGUSTA  BRUTALE 800_gallerypage");
+});
+onEvent("button7", "click", function() {
+  setScreen("mvagustabikes1");
+});
+onEvent("gallerymvagustaf3800", "click", function() {
+  setScreen("mvagustaf3800_gallerypage");
+});
+onEvent("backfrommvagustaf3800_gallerypage", "click", function() {
+  setScreen("mvagustabikes1");
+});
+onEvent("gallerymvagustabrutale1090", "click", function() {
+  setScreen("mvagustabrutale1090gallerypage");
+});
+onEvent("backfrommvagustabrutale1090gallerypage", "click", function() {
+  setScreen("mvagustabikes2");
+});
+onEvent("gallerymvagustabrutale1090rr", "click", function() {
+  setScreen("mvagustabrutale1090rrgallerypage");
+});
+onEvent("button11", "click", function() {
+  setScreen("mvagustabikes2");
+});
+onEvent("gallerymvagustaf4", "click", function() {
+  setScreen("mvagustaf4gallerypage");
+});
+onEvent("button12", "click", function() {
+  setScreen("mvagustabikes3");
+});
+onEvent("gallerymvagustaf4rr", "click", function() {
+  setScreen("mvagustaf4rrgallerypage");
+});
+onEvent("button13", "click", function() {
+  setScreen("mvagustabikes3");
+});
+onEvent("gallerymvagustaf4rc", "click", function() {
+  setScreen("mvagustaf4rcgallerypage");
+});
+onEvent("button14", "click", function() {
+  setScreen("mvagustabike4");
+});
+onEvent("button35", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("button41", "click", function() {
+  setScreen("mvagustabikes2");
+});
+onEvent("button49", "click", function() {
+  setScreen("mvagustabikes1");
+});
+onEvent("button50", "click", function() {
+  setScreen("mvagustabikes3");
+});
+onEvent("button58", "click", function() {
+  setScreen("mvagustabikes2");
+});
+onEvent("button59", "click", function() {
+  setScreen("mvagustabike4");
+});
+onEvent("backfrommvaugusta4", "click", function() {
+  setScreen("mvagustabikes3");
+});
+onEvent("categoryfrommvagusta4", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("ktm_image", "click", function() {
+  setScreen("ktmbikes1");
+});
+onEvent("TOP5BUTTON", "click", function() {
+  setScreen("top5");
+});
+onEvent("SUBMIT BTN", "click", function() {
+  createRecord("FEED BACK TABLE", {NAME :getText("NAME INPUT"),COMMENTS :getText("COMMENTS AREA")}, function(record) {
+    console.log("TABLE UPDATED ");
+  });
+});
+onEvent("FEEBACK BTN", "click", function() {
+  setScreen("FEEDBACKSCREEN");
+});
+onEvent("backfrombrand1", "click", function() {
+  setScreen("Home");
+});
+onEvent("nextfrombrand1", "click", function() {
+  setScreen("brandcategory2");
+});
+onEvent("homefrombrand2", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("SUBMIT BTN", "click", function() {
+  setScreen("THANK U SCREEN");
+});
+onEvent("THAN KU SCREEN BACK BTN", "click", function() {
+  setScreen("Home");
+});
+
+onEvent("dropdown1", "change", function() {
+  if (getText("dropdown1")=="YAMAHA YZF R1") {
+    setScreen("MY FAV BIKE 1");
+  } 
+  else if (getText("dropdown1")=="KAWASAKI ZX-10R") {
+    setScreen("MY FAV BIKE 2"); }
+  else if (getText("dropdown1")=="HAYABUSA") {
+    setScreen("MY FAV BIKE 3"); }
+  else if (getText("dropdown1")=="GSX S1000") {
+    setScreen("MY FAV BIKE 4"); }
+  else if (getText("dropdown1")=="H2R") {
+  setScreen("MY FAV BIKE 5"); 
+  } else {
+  setScreen("top5");  
+  }
+});
+onEvent("backfromfavbike1", "click", function() {
+  setScreen("top5");
+  setProperty("dropdown1", "value", "SELECT YOUR BIKE");
+});
+onEvent("backfromfavbike2", "click", function() {
+  setScreen("top5");
+  setProperty("dropdown1", "value", "SELECT YOUR BIKE");
+});
+onEvent("backfromfavbike3", "click", function() {
+  setScreen("top5");
+  setProperty("dropdown1", "value", "SELECT YOUR BIKE");
+});
+onEvent("backfromfavbike4", "click", function() {
+  setScreen("top5");
+  setProperty("dropdown1", "value", "SELECT YOUR BIKE");
+});
+onEvent("backfromfavbike5", "click", function() {
+  setScreen("top5");
+  setProperty("dropdown1", "value", "SELECT YOUR BIKE");
+});
+onEvent("button15", "click", function() {
+  setScreen("Brand_category1");
+});
+onEvent("backfromfeedback", "click", function() {
+  setScreen("Home");
+});
+
+
